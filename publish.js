7d242b32b4eb2ca3218705050b4c596a19061953
Luke Brooker <mail@lukebrooker.com>
2016-01-29 09:18:21 +1000

    Fix publish scripts

--- publish.js
+++ publish.js
@@ -3,29 +3,33 @@ var path = require('path')
 var fs = require('fs-extra')
 
 function copyCname (cb) {
-  fs.copy('/CNAME', '/public/CNAME', function (err) {
+  fs.copy('./CNAME', './public/CNAME', function (err) {
     if (err) return console.error(err)
     console.log('CNAME copied')
-    cb()
+    if (typeof cb === 'function') cb()
   })
 }
 
 function copyPresentations (cb) {
-  fs.copy('/presentations', '/public/presentations', function (err) {
+  fs.copy('./presentations', './public/presentations', function (err) {
     if (err) return console.error(err)
     console.log('Presentations Copied')
-    cb()
+    if (typeof cb === 'function') cb()
   })
 }
 
-function copyToBranch () {
+function copyToBranch (cb) {
   ghpages.publish(path.join(__dirname, 'public'), function (err) {
-    console.error(err)
+    if (err) return console.error(err)
+    console.log('Pushed to github pages')
+    if (typeof cb === 'function') cb()
   })
 }
 
 function publish () {
-  copyCname(copyPresentations(copyToBranch()))
+  copyCname(copyPresentations(copyToBranch(function () {
+    console.log('Published!')
+  })))
 }
 
 publish()
