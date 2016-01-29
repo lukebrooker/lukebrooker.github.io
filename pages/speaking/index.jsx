7422e2b1c81ed5ee60d71c19a8169d63fc1194c5
Luke Brooker <mail@lukebrooker.com>
2016-01-29 15:21:55 +1000

    Fix speaking list layout on small screens

--- pages/speaking/index.jsx
+++ pages/speaking/index.jsx
@@ -19,13 +19,13 @@ export default class Speaking extends Component {
         <div>
           {sortedPages.map((page, key) => {
             return (page.data && (page.path.indexOf('/speaking/') !== -1))
-              ? (<div key={key} className='D(f) W(100%) P(rh) Mb(r1)'>
-                  <div className='D(f) Fld(c) W(1/2)--sm Mend(rh)'>
-                    <h2 className='Fz(ms1) Fw(600)'><Link className='Link' to={page.path}>{page.data.title}</Link></h2>
+              ? (<div key={key} className='D(f)--sm W(100%) Mb(r1)'>
+                  <div className='D(f) Fld(c) W(100%) W(1/2)--sm Mend(rh)--sm'>
+                    <h2 className='Fz(ms1) Fw(600) Mb(rh)'><Link className='Link' to={page.path}>{page.data.title}</Link></h2>
                     <div className='Fz(msn1) Tt(u) Op(.6) Mb(rh)'>{moment(page.data.date).format('MMMM D, YYYY')}</div>
                   </div>
                   { page.data.teaserImage
-                    ? (<div className='W(1/2)--sm Mstart(rh)'>
+                    ? (<div className='W(100%) W(1/2)--sm Mstart(rh)--sm'>
                       <Link to={page.path}><img src={page.path + page.data.teaserImage} alt={page.data.title}/></Link>
                     </div>)
                     : undefined}
