adeea5c32f5cb90a2f0780158151d7868a0f6643
Luke Brooker <mail@lukebrooker.com>
2016-02-01 08:00:15 +1000

    Add craft as in beer to speaking

--- pages/speaking/index.jsx
+++ pages/speaking/index.jsx
@@ -16,22 +16,34 @@ export default class Speaking extends Component {
     }).reverse()
     return (
       <DocumentTitle title={'Speaking | ' + config.htmlTitle}>
-        <div>
-          {sortedPages.map((page, key) => {
-            return (page.data && (page.path.indexOf('/speaking/') !== -1))
-              ? (<div key={key} className='D(f)--sm W(100%) Mb(r1)'>
-                  <div className='D(f) Fld(c) W(100%) W(1/2)--sm Mend(rh)--sm'>
-                    <h2 className='Fz(ms1) Fw(600) Mb(rh)'><Link className='Link' to={page.path}>{page.data.title}</Link></h2>
-                    <div className='Fz(msn1) Tt(u) Op(.6) Mb(rh)'>{moment(page.data.date).format('MMMM D, YYYY')}</div>
+        <div className='Mx(nrq)--sm Mx(nrh)--md'>
+          {sortedPages
+            .filter((page) =>
+              (page.data && (page.path.indexOf('/speaking/') !== -1)))
+            .map((page, key) => {
+              const itemDir = (key % 2 === 0) ? '' : ' Fld(rr)--sm'
+              return (<div key={key}
+                className={'D(f)--sm W(100%) Mb(r1h) Mb(r2)--md' + itemDir}>
+                <div className='D(f) Fld(c) W(100%) W(1/2)--sm Px(rq)--sm Px(rh)--md'>
+                  <h2 className='Fz(ms1) Fw(600) Mb(rh)'>
+                    <Link className='Link'
+                      to={page.path}>{page.data.title}</Link>
+                  </h2>
+                  <div className='Fz(msn1) Tt(u) Op(.6) Mb(rh)'>
+                    {moment(page.data.date).format('MMMM D, YYYY')}
                   </div>
-                  { page.data.teaserImage
-                    ? (<div className='W(100%) W(1/2)--sm Mstart(rh)--sm'>
-                      <Link to={page.path}><img src={page.path + page.data.teaserImage} alt={page.data.title}/></Link>
-                    </div>)
-                    : undefined}
-                </div>)
-              : undefined
-          })}
+                </div>
+                { page.data.teaserImage
+                  ? (<div className='W(100%) W(1/2)--sm Px(rq)--sm Px(rh)--md'>
+                    <Link to={page.path}>
+                      <img src={page.path + page.data.teaserImage}
+                        alt={page.data.title}/>
+                    </Link>
+                  </div>)
+                  : undefined}
+              </div>)
+            }
+          )}
         </div>
       </DocumentTitle>
     )
