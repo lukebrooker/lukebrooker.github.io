01f60090e989b50d6e3434fde375bf6a53f544f0
Luke Brooker <mail@lukebrooker.com>
2024-04-15 01:13:18 +1000

    Remove line about more work coming

--- src/app/work/index.tsx
+++ src/app/work/index.tsx
@@ -106,9 +106,6 @@ export default function Work () {
             </YStack>
           )
         })}
-        <Paragraph>
-          More work coming soon.
-        </Paragraph>
       </MainContainer>
     </>
   )
